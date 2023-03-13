0cd2d99877f6c1fdae8f01c9f33ea7ca140b1b8a
Abhay Tiwari <abhay90458@gmail.com>
2023-03-13 20:00:58 +0530

    All Working

--- index.js
+++ index.js
@@ -146,8 +146,8 @@ app.post("/postArticle/Image", (req, res) => {
         method: "POST",
         data: {
             author: `urn:li:person:${req.body.id}`,
-            commentary: "test strings!",
-            visibility: "PUBLIC",
+            commentary: req.body.title,
+            visibility: req.body.scope,
             distribution: {
                 feedDistribution: "MAIN_FEED",
                 targetEntities: [],
@@ -155,7 +155,7 @@ app.post("/postArticle/Image", (req, res) => {
             },
             content: {
                 media: {
-                    altText: "testing for alt tags",
+                    altText: req.body.altText,
                     id: req.body.asset,
                 },
             },
@@ -215,12 +215,125 @@ app.post("/imageUpload", (0, multer_1.default)({ storage: multer_1.default.memor
             "Content-Type": (_b = req === null || req === void 0 ? void 0 : req.file) === null || _b === void 0 ? void 0 : _b.mimetype,
         },
     })
-        .then((response) => {
-        res.send("Posted image");
+        .then(() => {
+        res.send("Posted");
     })
         .catch((err) => {
         console.log(err);
         res.send(err);
+    });
+});
+//Video Apis
+app.post("/postVideoStepOne", (req, res) => {
+    console.log(req.body);
+    (0, axios_1.default)({
+        url: "https://api.linkedin.com/rest/videos?action=initializeUpload",
+        method: "POST",
+        data: {
+            initializeUploadRequest: {
+                owner: `urn:li:person:${req.body.id}`,
+                fileSizeBytes: 1055736,
+                uploadCaptions: false,
+                uploadThumbnail: false,
+            },
+        },
+        headers: {
+            Authorization: "Bearer " + req.body.accessToken,
+            "X-Restli-Protocol-Version": "2.0.0",
+            "Content-Type": "application/json",
+            "LinkedIn-Version": "202301",
+        },
+    })
+        .then((response) => {
+        res.send(response.data);
+    })
+        .catch((err) => {
+        console.log(err);
+        res.send(err);
+    });
+});
+app.post("/videoUploadStepTwo", (0, multer_1.default)({ storage: multer_1.default.memoryStorage() }).single("file"), (req, res) => {
+    var _a;
+    console.log(req.body);
+    console.log(req.file);
+    (0, axios_1.default)({
+        url: req.body.url,
+        method: "POST",
+        data: (_a = req.file) === null || _a === void 0 ? void 0 : _a.buffer,
+        headers: {
+            Authorization: "Bearer " + req.body.accessToken,
+            "Content-Type": " application/octet-stream",
+        },
+    })
+        .then((response) => {
+        res.send(response.headers.etag);
+    })
+        .catch((err) => {
+        console.log(err);
+        res.send(err);
+    });
+});
+app.post("/videoFinalizeStepThree", (req, res) => {
+    (0, axios_1.default)({
+        url: "https://api.linkedin.com/v2/videos?action=finalizeUpload",
+        method: "POST",
+        data: {
+            finalizeUploadRequest: {
+                video: req.body.asset,
+                uploadToken: "",
+                uploadedPartIds: [req.body.eTag],
+            },
+        },
+        headers: {
+            Authorization: "Bearer " + req.body.accessToken,
+        },
+    })
+        .then((response) => {
+        console.log(response);
+        res.send("Posted");
+    })
+        .catch((err) => {
+        console.log(err);
+        res.send(err);
+    });
+});
+app.post("/postArticle/Video", (req, res) => {
+    console.log(req.file);
+    (0, axios_1.default)({
+        url: "https://api.linkedin.com/rest/posts",
+        method: "POST",
+        data: {
+            author: `urn:li:person:${req.body.id}`,
+            commentary: req.body.title,
+            visibility: req.body.scope,
+            distribution: {
+                feedDistribution: "MAIN_FEED",
+                targetEntities: [],
+                thirdPartyDistributionChannels: [],
+            },
+            content: {
+                media: {
+                    title: req.body.videoTitle,
+                    id: req.body.asset,
+                },
+            },
+            lifecycleState: "PUBLISHED",
+            isReshareDisabledByAuthor: false,
+        },
+        headers: {
+            Authorization: "Bearer " + req.body.accessToken,
+            "Content-Type": "application/json",
+            "X-Restli-Protocol-Version": "2.0.0",
+            "LinkedIn-Version": "202301",
+        },
+    })
+        .then((response) => {
+        console.log(response.data);
+        res.send("completed");
+    })
+        .catch((e) => {
+        console.log(e);
+        res.send("Error");
     });
 });
 app.listen(port, () => {
--- index.ts
+++ index.ts
@@ -150,8 +150,8 @@ app.post("/postArticle/Image", (req, res) => {
     method: "POST",
     data: {
       author: `urn:li:person:${req.body.id}`,
-      commentary: "test strings!",
-      visibility: "PUBLIC",
+      commentary: req.body.title,
+      visibility: req.body.scope,
       distribution: {
         feedDistribution: "MAIN_FEED",
         targetEntities: [],
@@ -159,7 +159,7 @@ app.post("/postArticle/Image", (req, res) => {
       },
       content: {
         media: {
-          altText: "testing for alt tags",
+          altText: req.body.altText,
           id: req.body.asset,
         },
       },
@@ -224,8 +224,8 @@ app.post(
         "Content-Type": req?.file?.mimetype,
       },
     })
-      .then((response) => {
-        res.send("Posted image");
+      .then(() => {
+        res.send("Posted");
       })
       .catch((err) => {
         console.log(err);
@@ -233,6 +233,127 @@ app.post(
       });
   }
 );
+
+//Video Apis
+app.post("/postVideoStepOne", (req, res) => {
+  console.log(req.body);
+  axios({
+    url: "https://api.linkedin.com/rest/videos?action=initializeUpload",
+    method: "POST",
+    data: {
+      initializeUploadRequest: {
+        owner: `urn:li:person:${req.body.id}`,
+        fileSizeBytes: 1055736,
+        uploadCaptions: false,
+        uploadThumbnail: false,
+      },
+    },
+    headers: {
+      Authorization: "Bearer " + req.body.accessToken,
+      "X-Restli-Protocol-Version": "2.0.0",
+      "Content-Type": "application/json",
+      "LinkedIn-Version": "202301",
+    },
+  })
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send(err);
+    });
+});
+
+app.post(
+  "/videoUploadStepTwo",
+  multer({ storage: multer.memoryStorage() }).single("file"),
+  (req, res) => {
+    console.log(req.body);
+    console.log(req.file);
+
+    axios({
+      url: req.body.url,
+      method: "POST",
+      data: req.file?.buffer,
+      headers: {
+        Authorization: "Bearer " + req.body.accessToken,
+        "Content-Type": " application/octet-stream",
+      },
+    })
+      .then((response) => {
+        res.send(response.headers.etag);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.send(err);
+      });
+  }
+);
+
+app.post("/videoFinalizeStepThree", (req, res) => {
+  axios({
+    url: "https://api.linkedin.com/v2/videos?action=finalizeUpload",
+    method: "POST",
+    data: {
+      finalizeUploadRequest: {
+        video: req.body.asset,
+        uploadToken: "",
+        uploadedPartIds: [req.body.eTag],
+      },
+    },
+    headers: {
+      Authorization: "Bearer " + req.body.accessToken,
+    },
+  })
+    .then((response) => {
+      console.log(response);
+      res.send("Posted");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send(err);
+    });
+});
+
+app.post("/postArticle/Video", (req, res) => {
+  console.log(req.file);
+  axios({
+    url: "https://api.linkedin.com/rest/posts",
+    method: "POST",
+    data: {
+      author: `urn:li:person:${req.body.id}`,
+      commentary: req.body.title,
+      visibility: req.body.scope,
+      distribution: {
+        feedDistribution: "MAIN_FEED",
+        targetEntities: [],
+        thirdPartyDistributionChannels: [],
+      },
+      content: {
+        media: {
+          title: req.body.videoTitle,
+          id: req.body.asset,
+        },
+      },
+      lifecycleState: "PUBLISHED",
+      isReshareDisabledByAuthor: false,
+    },
+    headers: {
+      Authorization: "Bearer " + req.body.accessToken,
+      "Content-Type": "application/json",
+      "X-Restli-Protocol-Version": "2.0.0",
+      "LinkedIn-Version": "202301",
+    },
+  })
+    .then((response) => {
+      console.log(response.data);
+      res.send("completed");
+    })
+    .catch((e) => {
+      console.log(e);
+      res.send("Error");
+    });
+});
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}/`);
